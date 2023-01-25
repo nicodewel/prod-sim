f8d6f82483100208deaba3543b408c0e931a87e9
Nicolas Weltzel <nwelt13@gmail.com>
2023-01-25 14:10:49 +0100

    Service und Endpunkte für CarModel erstellt

---
+++ production-backend/src/main/java/de/volkswagen/productionbackend/controller/CarModelController.java
@@ -0,0 +1,38 @@
+package de.volkswagen.productionbackend.controller;
+
+import de.volkswagen.productionbackend.model.CarModel;
+import de.volkswagen.productionbackend.service.CarModelService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+public class CarModelController {
+
+    private final CarModelService carModelService;
+
+    public CarModelController(CarModelService carModelService) {
+        this.carModelService = carModelService;
+    }
+
+    @GetMapping("/carModels")
+    public ResponseEntity<List<CarModel>> getAll(){
+        return ResponseEntity.ok(carModelService.getAllCarModels());
+    }
+
+    @GetMapping("/carModels/{id}")
+    public ResponseEntity<CarModel> getById(@PathVariable long id){
+        if (carModelService.getCarModelById(id).isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(carModelService.getCarModelById(id).get());
+    }
+
+    @PostMapping("/carModels")
+    public ResponseEntity<CarModel> save(@RequestBody CarModel carModel){
+        return ResponseEntity.ok(carModelService.saveCarModel(carModel));
+    }
+
+
+}
