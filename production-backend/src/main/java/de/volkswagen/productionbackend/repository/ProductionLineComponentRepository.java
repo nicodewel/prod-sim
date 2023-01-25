6f0ebec85b1526242dab5fe110113b25cdc7310f
Nicolas Weltzel <nwelt13@gmail.com>
2023-01-25 14:14:23 +0100

    Service und Endpunkte für ProductionLine erstellt

--- production-backend/src/main/java/de/volkswagen/productionbackend/repository/ProductionLineComponentRepository.java
+++ production-backend/src/main/java/de/volkswagen/productionbackend/repository/ProductionLineComponentRepository.java
@@ -1,7 +1,8 @@
 package de.volkswagen.productionbackend.repository;
 
 import de.volkswagen.productionbackend.model.ProductionLineComponent;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductionLineComponentRepository extends CrudRepository<ProductionLineComponent,Long> {
+public interface ProductionLineComponentRepository extends JpaRepository<ProductionLineComponent,Long> {
 }
