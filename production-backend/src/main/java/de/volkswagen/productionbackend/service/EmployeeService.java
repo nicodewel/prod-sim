a7d51342b3a859c8209b215281dac33a3cd16921
Nicolas Weltzel <nwelt13@gmail.com>
2023-01-25 14:07:33 +0100

    Service und Endpunkte für Employee erstellt

---
+++ production-backend/src/main/java/de/volkswagen/productionbackend/service/EmployeeService.java
@@ -0,0 +1,34 @@
+package de.volkswagen.productionbackend.service;
+
+import de.volkswagen.productionbackend.model.Employee;
+import de.volkswagen.productionbackend.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(long id){
+        return employeeRepository.findById(id);
+    }
+
+    public void deleteEmployeeById(long id){
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee saveEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+}
