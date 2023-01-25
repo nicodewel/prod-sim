cf9e96a0d0fc21e04774672786fb5557fbc8cac5
Nicolas Weltzel <nwelt13@gmail.com>
2023-01-25 09:09:16 +0100

    Spring initializer ausgeführt

---
+++ backend/production-backend/src/main/java/de/volkswagen/productionbackend/ProductionBackendApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.productionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionBackendApplication.class, args);
+	}
+
+}
---
+++ backend/production-backend/src/test/java/de/volkswagen/productionbackend/ProductionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.volkswagen.productionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
